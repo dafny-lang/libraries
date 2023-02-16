3ee5d7ccd79a1a74329ab7e1e6835c0104a203cb
stefan-aws <stefanze@amazon.com>
2023-02-16 14:08:16 +0000

    type descriptor

--- src/MutableMap/MutableMap.java
+++ src/MutableMap/MutableMap.java
@@ -7,17 +7,25 @@ package MutableMap;
 import dafny.DafnySet;
 import dafny.DafnyMap;
 import dafny.Tuple2;
+import dafny.TypeDescriptor;
 
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
 
 public class MutableMap<K,V> {
   private HashMap<K, V> m;
 
-  public DafnyMap<? extends Tuple2<K,V>> content() {
+  public DafnyMap<K,V> content() {
     return new DafnyMap<>(m);
   }
 
   public MutableMap() {
+    m = new HashMap<>();
+  }
+
+  public MutableMap(TypeDescriptor<K> x, TypeDescriptor<V> y) {
     m = new HashMap<>();
   }
 
@@ -35,14 +43,14 @@ public class MutableMap<K,V> {
 
   public DafnySet<? extends Tuple2<K,V>> items() {
     ArrayList<Tuple2<K, V>> list = new ArrayList<Tuple2<K, V>>();
-    for (Entry<K, V> entry : m.entrySet()) {
+    for (Map.Entry<K, V> entry : m.entrySet()) {
       list.add(new Tuple2<K, V>(entry.getKey(), entry.getValue()));
     }
     return new DafnySet<Tuple2<K, V>>(list);
   }
 
   public V find(K k) {
-    m.get(k);
+    return m.get(k);
   }
 
   public void remove(K k) {
